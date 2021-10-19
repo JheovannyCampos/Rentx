6c8e3aa32310cb460aca23084a981e5fd7f17fad
Jheovanny Campos <jheovanny06@hotmail.com>
2021-10-19 12:14:20 -0300

    git commit -m "interface de detalhes"

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -7,6 +7,15 @@ import {
     Container,
     Header,
     CarImages,
+    Content,
+    Details,
+    Description,
+    Brand,
+    Name,
+    Rent,
+    Period,
+    Price,
+    About,
 } from './styles';
 
 export function CarDetails() {
@@ -20,6 +29,23 @@ export function CarDetails() {
                     imagesUrl={['https://production.autoforce.com/uploads/version/profile_image/3188/model_main_comprar-tiptronic_87272c1ff1.png']}
                 />
             </CarImages>
+
+            <Content>
+                <Details>
+                    <Description>
+                        <Brand>Lamborghini</Brand>
+                        <Name>Huracan</Name>
+                    </Description>
+
+                    <Rent>
+                        <Period>Ao Dia</Period>
+                        <Price>R$ 580</Price>
+                    </Rent>
+                </Details>
+                <About>
+                    Este é automóvel desportivo. Surgiu do lendário touro de lide indultado na praça Real Maestranza de Sevilla. É um belíssimo carro para quem gosta de acelerar.
+                </About>
+            </Content>
         </Container>
     );
 }
