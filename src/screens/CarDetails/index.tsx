640f6bd96945f76c3538e25b7ca24fb84d8f90e4
Jheovanny Campos <jheovanny06@hotmail.com>
2021-10-19 11:39:33 -0300

    git commit -m "Componente de Slider"

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+
 import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
 
 import {
     Container,
     Header,
+    CarImages,
 } from './styles';
 
 export function CarDetails() {
@@ -12,6 +15,11 @@ export function CarDetails() {
             <Header>
                 <BackButton onPress={() =>{}}/>
             </Header>
+            <CarImages>
+                <ImageSlider 
+                    imagesUrl={['https://production.autoforce.com/uploads/version/profile_image/3188/model_main_comprar-tiptronic_87272c1ff1.png']}
+                />
+            </CarImages>
         </Container>
     );
 }
